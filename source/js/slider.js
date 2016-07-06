bd58d6d14aba91ee1ce6f19cfc727985e667e958
AcidHardcore <vitcore@mail.ru>
2016-07-06 15:54:54 +0300

    added js function for changing colors of body and buttons

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,19 +1,89 @@
 // var butPrev = document.querySelector('.review__prev-js');
 // var butNext = document.querySelector('.review__next-js');
+var body = document.querySelector('body');
+var search = document.querySelector('.btn--search');
+var entrance = document.querySelector('.btn--entrance');
+var cart = document.querySelector('.btn--cart');
 
-//create sliderReview
-// var sliderReview = Peppermint(document.getElementById('peppermint'), {
-//     dots: true,
-//     slideshow: true,
-//     speed: 500,
-//     slideshowInterval: 5000,
-//     stopSlideshowAfterInteraction: true,
-//     dotsContainer: document.querySelector('.slider__paginator')
-//     //dotsPrepend: true //dots above, false: dots below
-//     // onSetup: function(n) {
-//     //     console.log('Peppermint setup done. Slides found: ' + n);
-//     // }
-// });
+// create sliderReview
+var slider = Peppermint(document.getElementById('peppermint'), {
+    dots: true,
+    slideshow: true,
+    speed: 500,
+    slideshowInterval: 5000,
+    stopSlideshowAfterInteraction: true,
+    cssPrefix: 'slider__',
+    onSlideChange: changeBackground
+    // dotsContainer: document.querySelector('.slider__paginator')
+    //dotsPrepend: true //dots above, false: dots below
+    // onSetup: function(n) {
+    //     console.log('Peppermint setup done. Slides found: ' + n);
+    // }
+});
+
+function changeBackground (activeSlide){
+    changeBodyBackground (activeSlide);
+    changeSearchBtnBackground (activeSlide);
+    changeEntranceBtnBackground (activeSlide);
+    changeCartBtnBackground (activeSlide);
+}
+
+function changeBodyBackground(activeSlide) {
+    var slideBodyBgClass = [
+        'body-bgc-1-js',
+        'body-bgc-2-js',
+        'body-bgc-3-js'
+    ];
+
+    slideBodyBgClass.forEach(function (el, i) {
+        body.classList.remove(el);
+    });
+
+    body.classList.add(slideBodyBgClass[activeSlide]);
+}
+
+function changeSearchBtnBackground(activeSlide) {
+    var slideSearchBtnClass = [
+        'btn--search-bgc-1-js',
+        'btn--search-bgc-2-js',
+        'btn--search-bgc-3-js'
+    ];
+
+    slideSearchBtnClass.forEach(function (el, i) {
+        search.classList.remove(el);
+    });
+
+    search.classList.add(slideSearchBtnClass[activeSlide]);
+}
+
+function changeEntranceBtnBackground(activeSlide) {
+    var slideEntranceBtnClass = [
+        'btn--entrance-bgc-1-js',
+        'btn--entrance-bgc-2-js',
+        'btn--entrance-bgc-3-js'
+    ];
+
+    slideEntranceBtnClass.forEach(function (el, i) {
+        entrance.classList.remove(el);
+    });
+
+    entrance.classList.add(slideEntranceBtnClass[activeSlide]);
+}
+
+function changeCartBtnBackground(activeSlide) {
+    var slideCartBtnBgClass = [
+        'btn--cart-bgc-1-js',
+        'btn--cart-bgc-2-js',
+        'btn--cart-bgc-3-js'
+    ];
+
+    slideCartBtnBgClass.forEach(function (el, i) {
+        cart.classList.remove(el);
+    });
+
+    cart.classList.add(slideCartBtnBgClass[activeSlide]);
+}
+
 
 //sliderReview prev button
 // butPrev.addEventListener('tap', function (event) {
@@ -29,78 +99,3 @@
 //
 // });
 
-/*
- butPrev.addEventListener('tap', slider.prev, false);
- butNext.addEventListener('tap', slider.next, false);*/
-
-//Create sliderPrice
-// var sliderPrice = Peppermint(document.getElementById('sliderprice'), {
-//     dots: true,
-//     slideshow: false,
-//     speed: 500,
-//     slideshowInterval: 5000,
-//     startSlide: 1,
-//     stopSlideshowAfterInteraction: true
-    // onSetup: function(n) {
-    //     console.log('Peppermint setup done. Slides found: ' + n);
-    // }
-// });
-
-
-
-// Настройки
-
-
-/*
-{
-    //скорость перехода между слайдами, мс
-    speed: 300,
-
-        //скорость перехода между слайдами после тача, мс
-        touchSpeed: 300,
-
-    //включить слайдшоу
-    slideshow: false,
-
-    //интервал переключения слайдов, мс
-    slideshowInterval: 4000,
-
-    //останавливать слайдшоу после переключения слайда пользователем
-    stopSlideshowAfterInteraction: false,
-
-    //начальный слайд
-    startSlide: 0,
-
-    //разрешить управление мышкой
-    mouseDrag: true,
-
-    //не включать Peppermint, если слайд один
-    disableIfOneSlide: true,
-
-    //Префикс для служебных классов слайдера,
-    //таких как `inactive`, `active`, `mouse`, `drag` и т. д.
-    //Не забудьте поменять стили соответствующим образом!
-    cssPrefix: 'peppermint-',
-
-    //показывать точки
-    dots: false,
-
-    //положить точки в начало блока `dotsContainer` (по умолчанию кладутся в конец)
-    dotsPrepend: false,
-
-    //Элемент, в который положить точки. По умолчанию корневой элемент слайдера.
-    //Может быть где угодно на странице.
-    dotsContainer: undefined,
-
-    //Элемент, содержащий слайды. По умолчанию корневой элемент слайдера.
-    slidesContainer: undefined,
-
-    //Callback-функция, вызывается при смене слайда.
-    //В качестве параметра получает номер слайда.
-    onSlideChange: undefined,
-
-    //Callback-функция, вызывается пойсле завершения установки.
-    //В качестве параметра получает количество слайдов.
-    onSetup: undefined
-}
-*/
