b964f5a5301b56df63ff76f1a868906a8c25e734
AcidHardcore <vitcore@mail.ru>
2016-07-07 22:44:14 +0300

    amended hover for product block

--- source/js/slider.js
+++ source/js/slider.js
@@ -13,7 +13,7 @@ var slider = Peppermint(document.getElementById('peppermint'), {
     slideshowInterval: 5000,
     stopSlideshowAfterInteraction: true,
     cssPrefix: 'slider__',
-    onSlideChange: changeBackground
+    onSlideChange: changeBodyBackground
     // dotsContainer: document.querySelector('.slider__paginator')
     //dotsPrepend: true //dots above, false: dots below
     // onSetup: function(n) {
@@ -21,12 +21,12 @@ var slider = Peppermint(document.getElementById('peppermint'), {
     // }
 });
 
-function changeBackground (activeSlide){
-    changeBodyBackground (activeSlide);
-    changeSearchBtnBackground (activeSlide);
-    changeEntranceBtnBackground (activeSlide);
-    changeCartBtnBackground (activeSlide);
-}
+// function changeBackground (activeSlide){
+//     changeBodyBackground (activeSlide);
+//     changeSearchBtnBackground (activeSlide);
+//     changeEntranceBtnBackground (activeSlide);
+//     changeCartBtnBackground (activeSlide);
+// }
 
 function changeBodyBackground(activeSlide) {
     var slideBodyBgClass = [
@@ -41,49 +41,6 @@ function changeBodyBackground(activeSlide) {
 
     body.classList.add(slideBodyBgClass[activeSlide]);
 }
-
-function changeSearchBtnBackground(activeSlide) {
-    var slideSearchBtnClass = [
-        'btn--search-bgc-1-js',
-        'btn--search-bgc-2-js',
-        'btn--search-bgc-3-js'
-    ];
-
-    slideSearchBtnClass.forEach(function (el, i) {
-        search.classList.remove(el);
-    });
-
-    search.classList.add(slideSearchBtnClass[activeSlide]);
-}
-
-function changeEntranceBtnBackground(activeSlide) {
-    var slideEntranceBtnClass = [
-        'btn--entrance-bgc-1-js',
-        'btn--entrance-bgc-2-js',
-        'btn--entrance-bgc-3-js'
-    ];
-
-    slideEntranceBtnClass.forEach(function (el, i) {
-        entrance.classList.remove(el);
-    });
-
-    entrance.classList.add(slideEntranceBtnClass[activeSlide]);
-}
-
-function changeCartBtnBackground(activeSlide) {
-    var slideCartBtnBgClass = [
-        'btn--cart-bgc-1-js',
-        'btn--cart-bgc-2-js',
-        'btn--cart-bgc-3-js'
-    ];
-
-    slideCartBtnBgClass.forEach(function (el, i) {
-        cart.classList.remove(el);
-    });
-
-    cart.classList.add(slideCartBtnBgClass[activeSlide]);
-}
-
 
 //sliderReview prev button
 // butPrev.addEventListener('tap', function (event) {
