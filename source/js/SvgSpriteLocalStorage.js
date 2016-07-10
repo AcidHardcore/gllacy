690f05c472d56adfea28f477695fea508e141754
AcidHardcore <vitcore@mail.ru>
2016-07-10 14:57:14 +0300

    finished about block

--- source/js/SvgSpriteLocalStorage.js
+++ source/js/SvgSpriteLocalStorage.js
@@ -2,7 +2,7 @@
     'use strict';
 
     var file = 'img/sprite-svg--ls.svg',
-        revision = 27;
+        revision = 28;
 
     if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect)
         return true;
